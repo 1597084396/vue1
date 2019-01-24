f2d0c71eb6da65f78da0fb37a7aa2bae8d6db6bb
1597084396 <wenzhoubus@qq.com>
2019-01-24 20:37:10 +0800

    iconfont

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import store from './store'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import './assets/style/reset.css'
+import './assets/style/border.css'
+import './assets/style/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="header">
     <div class="header-left">
-        <div class="iconfont back-icon">&#xe624;</div>
+      <div class="iconfont back-icon">&#xe679;</div>
     </div>
     <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩/主题
+      <span class="iconfont">&#xe65c;</span>
+      输入城市/景点/游玩/主题
     </div>
-        <div class="header-right">
-          城市
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe661;</span>
+    </div>
   </div>
 </template>
 
@@ -21,35 +21,47 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-    @import '../../../assets/style/varibles.styl'
-    .header
-        display: flex
-        line-height: $headerHeight
-        background: $bgColor
-        color: #fff
-        .header-left
-            width: .64rem
-            float: left
-            .back-icon
-                text-align: center
-                font-size: .4rem
-        .header-input
-            flex: 1
-            background: #fff
-            border-radius: .1rem
-            height: .64rem
-            line-height: .64rem
-            margin-left: .2rem
-            margin-top: .12rem
-            padding-left: .2rem
-            color: #ccc
-        .header-right
-            min-width: 1.04rem
-            padding 0 .1rem
-            float: right
-            text-align: center
-            color: #fff
-            .arrow-icon
-                margin-left: -.04rem
-                font-size: .24rem  
+@import '../../../assets/style/varibles.styl';
+
+.header {
+  display: flex;
+  line-height: $headerHeight;
+  background: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    background: #fff;
+    border-radius: 0.1rem;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-left: 0.2rem;
+    margin-top: 0.12rem;
+    padding-left: 0.2rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    min-width: 1.04rem;
+    padding: 0 0.1rem;
+    float: right;
+    text-align: center;
+    color: #fff;
+
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
 </style>
